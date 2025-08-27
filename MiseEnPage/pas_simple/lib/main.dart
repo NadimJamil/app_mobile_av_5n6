4caa8cf6f8f057c41cbd2f1e55468299c6d5a597
Nadim Jamil <2261561@cegepmontpetit.ca>
2025-08-27 13:45:50 -0400

    FCT Fin pas si simple

--- MiseEnPage/pas_simple/lib/main.dart
+++ MiseEnPage/pas_simple/lib/main.dart
@@ -42,43 +42,54 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
 
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 200,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Expanded(flex:3,child: Container(
-                      color : Colors.red),
-                  ),
-                  const SizedBox(width: 8),
-                  Text("Test"),
-                  const SizedBox(width: 8),
-                  Container(
-                    color: Colors.black,
-                    width: 50,
-                    height: 90,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Expanded(flex: 1, child:
-          Row(
-              children: []
-          ),
-          ),
-          Expanded(flex: 1, child:
-          Row(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-
-              ]
-          ),
-          )
-        ],
+                Expanded(
+                  flex: 3,
+                    child: Container(
+                  width: 150,
+                  height: 200,
+                  color: Colors.red,
+                )),
+                Expanded(
+                  flex: 1,
+                    child: Container(
+                  child: Text("Yo", style: TextStyle(color: Colors.red, fontSize: 24),),
+                )),
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.black,
+                      height: 100,
+                    ))
+              ],
+            ),
+            Expanded(flex: 1, child:
+            Row(
+                children: []
+            ),
+            ),
+            Expanded(flex: 1, child:
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Expanded(flex: 1 ,child:
+                  Container(
+                    color: Colors.blue,
+                    child: TextButton(onPressed: () {}, child: const Text("Button du bas", style: TextStyle(color: Colors.white),)),
+                  ),
+                  ),
+                  // TextButton(onPressed: () {}, child: const Text("Boutton du bas", style: TextStyle(color: Colors.white),))
+                ]
+            ),
+            )
+          ],
+        ),
       ),
     );
   }
