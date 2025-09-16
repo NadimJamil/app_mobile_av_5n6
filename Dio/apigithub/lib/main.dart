7c232ef39ec438b859633e9da760961e4915a8d7
Nadim Jamil <2261561@cegepmontpetit.ca>
2025-09-16 17:43:30 -0400

    FCT : Fin Exercice api_github

--- Dio/apigithub/lib/main.dart
+++ Dio/apigithub/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,6 +30,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool? isPrivate;
+  String? githubUsername;
+  TextEditingController usernameController = TextEditingController();
+  void getHTTP(String nom) async{
+    try {
+      var response = await Dio().get("https://api.github.com/users/" + nom + "/repos");
+
+      if(response.statusCode == 200){
+        List<dynamic> repos = response.data;
+
+        for(var repo in repos){
+          setState(() {
+            isPrivate = repo['private'];
+            String githubUsername = repo['name'];
+          });
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,11 +63,27 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
+              controller: usernameController,
               decoration: const InputDecoration(
                 border: UnderlineInputBorder(),
-                labelText: 'Enter your username',
+                labelText: 'Enter your Github Username',
               ),
             ),
+            ElevatedButton(onPressed: (){
+              String username = usernameController.text;
+              if (username.isNotEmpty) {
+                setState(() {
+                  getHTTP(username);
+                });
+              } else {
+                setState(() {
+                  isPrivate = null;
+                  githubUsername = "No username entered.";
+                });
+              }
+            }, child: Text("Appuyez pour faire une requête")),
+            SizedBox(height: 32),
+            Text(usernameController.text + " account is private? : $isPrivate")
           ],
         ),
       ),
